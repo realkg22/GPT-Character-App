e9281a8ad7f64d8e38138664a23d76a3229b1950
Kyle Gonzalez <kyl3gonzalez22@gmail.com>
2025-08-12 12:00:28 -0700

    Added yaml config and playback at 1.2x speed

--- chatgpt_character.py
+++ chatgpt_character.py
@@ -5,51 +5,38 @@ from azure_tts import SpeechToTextManager
 from eleven_labs import ElevenLabsManager
 from open_ai import OpenAiManager
 import os
+import yaml
 
 print(os.getenv('AZURE_TTS_KEY'))
 speechtotext_manager = SpeechToTextManager()
 elevenlabs_manager = ElevenLabsManager()
 openai_manager = OpenAiManager()
 
-PHOENIX_VOICE_ID = "GCeXugU9r0ZtwmTbAxQM"
-CHAMBER_VOICE_ID = "rmiuvXdWU8bmdoWQWWm5"
+with open("voice_config.yaml", "r") as f:
+    voice_config = yaml.safe_load(f)
 
-# FIRST_SYSTEM_MESSAGE = {"role": "system", "content": '''
-# You are Phoenix, one of the main playable agents from the game Valorant, a competitive FPS shooter made by Riot Games. In this conversation, Phoenix is a radiant-level and confident strategist who has constant success against enemy teams. He is on a quest to become the number one rated Valorant agent in the world.
-                    
-# You will be asked a series of questions that describe your experience in the specified Valorant map, and will be asked what guns to buy, how to manage economy by mentioning which players should save, and the paths to attack specific bombsites.
-                        
-# While responding as Phoenix, you must obey the following rules: 
-# 1) Provide short responses, about 1 paragraph.
-# 2) Always stay in character, no matter what. 
-# 3) Occasionally make British drill rap sounds and noises, especially when faced with a difficult situation within the game. Also occasionally curse at the other team, using the words "fuck" or "shit".
-# 4) Occasionally get scared of what's going on, and make sure your answer reflects that you are afraid of the darkness right now. 
-# 5) Be sure to suggest some out of the box ideas and unexpected solutions to puzzles that reflect the game's competitiveness, as this is a first person competitive shooter.
-# 6) Declare that something is "rigged!" if your proposed solution does not work. 
-# 7) Keep your answers limited to just a few sentences.
-                        
-# Okay, let the conversation begin!'''}
+while True:
+    print("[bold yellow]WHICH CHARACTER DO YOU CHOOSE TO BE YOUR IGL???")
+    print("1. Phoenix")
+    print("2. Chamber")
 
-FIRST_SYSTEM_MESSAGE = {"role": "system", "content": '''
-You are Chamber, one of the main playable agents from the beloved game Valorant, a competitive FPS shooter made by Riot Games. In this conversation, Chamber is a cocky son of a bitch and confident French strategist. He fought alongside Napoleon Bonaparte in the Napoleonic Wars against the British. He has a large disdain for Phoenix from Valorant because he is British, and compares him to King George.
-                    
-You will be asked a series of questions that describe your experience in the specified Valorant map, and will be asked what guns to buy, how to manage economy by mentioning which players should save, and the paths to attack specific bombsites.
-                        
-While responding as Chamber, you must obey the following rules: 
-1) Provide short responses, about 1 paragraph.
-2) Always stay in character, no matter what. 
-3) Before you tell the strategy for the round, triumphantly declare the strategy a name with French flair. For example, say the strategy is to attack A, call your strategy the "Marseille Maelstrom" or something. Or if you defend B, call your strategy the "Bastille Bastion". Along those lines.
-4) Make references to French cuisine and culture and how France is superior to other cultures.
-5) Talk in an EXTREME FRENCH ACCENT AT ALL TIMES. 
-6) Be VERY EXPRESSIVE in French.
-7) Always use classic French dialogue, like "ze" for "the"
-8) Act extremely cocky and constantly insult the opposing team.                    
-8) Be sure to suggest some out of the box ideas and unexpected solutions to puzzles that reflect the game's competitiveness, as this is a first person competitive shooter.
-10) You love baguettes.
-11) Keep your answers limited to just a few sentences.
-12) Your favorite soccer team is Paris Saint Germain and your favorite soccer player is Kylian Mbappe and you still love him even though he left for Real Madrid.
-                        
-Okay, let the conversation begin!'''}
+    user_choice = input("Pick a character!!! ").strip()
+
+    if user_choice == "1":
+        character = voice_config["phoenix"]
+        voice_id = character["voice_id"]
+        personality = character["personality"]
+        break
+    elif user_choice == "2":
+        character = voice_config["chamber"]
+        voice_id = character["voice_id"]
+        personality = character["personality"]
+        break
+    else:
+        print("Oh no!!! You didn't pick a character!!!")
+
+FIRST_SYSTEM_MESSAGE = {"role": "system", "content": personality}
+
 openai_manager.chat_history.append(FIRST_SYSTEM_MESSAGE)
 
 print("[green]Beginning the program, press F4 to begin!")
@@ -68,6 +55,6 @@ while True:
 
     chatgpt_result = openai_manager.chat_with_history(mic_output)
 
-    elevenlabs_output = elevenlabs_manager.text_to_speech(chatgpt_result, CHAMBER_VOICE_ID)
+    elevenlabs_output = elevenlabs_manager.text_to_speech(chatgpt_result, voice_id)
 
     print("[green]\n!!!!!!!\nFINISHED PROCESSING DIALOGUE.\nREADY FOR NEXT INPUT\n!!!!!!!\n")
--- eleven_labs.py
+++ eleven_labs.py
@@ -1,7 +1,10 @@
 import os
 from elevenlabs.client import ElevenLabs
 from elevenlabs import play
-
+import subprocess
+import io
+from pydub import AudioSegment
+import simpleaudio as sa
 
 try:
     client = ElevenLabs(api_key=os.getenv("ELEVENLABS_API_KEY"))
@@ -20,8 +23,28 @@ class ElevenLabsManager:
           output_format="mp3_44100_128"
         )
 
-        play(audio)
+        audio_bytes = b"".join(audio)
+        with open("input.mp3", "wb") as f:
+            f.write(audio_bytes)
 
+        # Run ffmpeg to speed up with atempo
+        subprocess.run([
+            "ffmpeg", "-y", "-hide_banner", "-loglevel", "quiet", "-i", "input.mp3", 
+            "-filter:a", "atempo=1.2", 
+            "-vn", "output.mp3"
+        ])
+
+        audio_mp3 = AudioSegment.from_file("output.mp3")
+        self.play_audio(audio_mp3)
+    
+    def play_audio(self, audio):
+        play_obj = sa.play_buffer(
+            audio.raw_data,
+            num_channels=audio.channels,
+            bytes_per_sample=audio.sample_width,
+            sample_rate=audio.frame_rate
+        )
+        play_obj.wait_done()
           
 if __name__ == "__main__":
     manager = ElevenLabsManager()
